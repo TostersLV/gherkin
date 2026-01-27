f0b55a0910922cc906c6fc885e497a4c0f0f7c7e
TostersLV <rpeizums@gmail.com>
2026-01-27 09:31:06 +0200

    pabeigts (kinda)

--- src/HappyPathScenario.jsx
+++ src/HappyPathScenario.jsx
@@ -1,7 +1,7 @@
 function HappyPathScenario() {
     return (
         <article>
-            <div className="border bg-slate-700 rounded-lg mx-2 my-2">
+            <div className="border bg-slate-800 rounded-lg mx-2 my-2">
                 <div className="mx-2 p-2">
                 <div className="text-white text-lg font-bold">Kas ir veiksmīgā iznākuma scenārijs (angļu v. Happy Path Scenario)?</div>
                 <p className="text-white">Veiksmīgā iznākuma scenārijs ir ka funkcionālās iezīmes autors/izstrādātājs ir izpildījis 
--- src/RubicCube.jsx
+++ src/RubicCube.jsx
@@ -1,7 +1,7 @@
 function RubicCube() {
     return (
         <article>
-            <div className="border rounded-lg mx-2 my-2 bg-slate-800">
+            <div className="border rounded-lg mx-2 my-2 bg-slate-700">
                 <div className="mx-2 p-2">
                 <div className="text-white text-lg font-bold">Kā Rubika kubs ļauj skatīties uz funkcionālajām iezīmēm?</div>
                 <p className="text-white">Viena iezīme paliek nemainīga, bet, skatoties no dažādiem leņķiem, 
